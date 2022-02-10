b61a2b7533b202c273b44f756bd44904278c3a13
deepeters <dennis95peters@gmail.com>
2022-02-10 10:09:23 +0300

    Add project files.

---
+++ trial_episerver/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using trial_episerver.Models.Pages;
+
+namespace trial_episerver.Controllers
+{
+    public class HomeController : PageController<Home>
+    {
+        // GET: Home
+        public ActionResult Index(Home currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
